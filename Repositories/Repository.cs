da3a2a3136293a30191e4019f59e8bcad8f3a919
root <root@xarras>
2024-04-07 17:50:58 -0300

    Repositório genérico criado

---
+++ Repositories/Repository.cs
@@ -0,0 +1,34 @@
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+namespace Blog.Repositories
+{
+    public class Repository<T> where T : class // using "<T>" on the final of our class we define a generic class
+    {
+        // It isn't interesting to us pass a conection parameter in each class
+        // so we remove it from the methods and pass as a private variable
+        private readonly SqlConnection _connection; // private variable to work with the connection inside the class     
+        public Repository(SqlConnection connection) => _connection = connection; // constructor method receive the external connection
+
+        // methods: in these methods we beguin to use a concept called "Generic"
+        // where we dont have to pass which object we are basing our method
+
+
+        public IEnumerable<T> Get() => _connection.GetAll<T>();
+        public T Get(int id) => _connection.Get<T>(id); // read one user if receive a id
+        public void Create(T model) => _connection.Insert<T>(model); // create a new user when receive the user's props
+
+        public void Update(T model) => _connection.Update<T>(model);// update user
+
+
+        public void Delete(T model) => _connection.Delete<T>(model);// delete user
+
+        public void Delete(int id) // delete user using just the id
+        {
+            if (id != 0)
+                return;
+            var model = _connection.Get<T>(id);
+            _connection.Delete<T>(model);
+        }
+    }
+}
--- Repositories/RoleRepository.cs
+++ Repositories/RoleRepository.cs
@@ -15,5 +15,29 @@ namespace Blog.Repositories
         public IEnumerable<Role> Get() => _connection.GetAll<Role>(); // read a list of Roles if don't receive a id
         public Role Get(int id) => _connection.Get<Role>(id); // read one Role if receive a id
         public void Create(Role role) => _connection.Insert<Role>(role); // create a new Role when receive the Role's props
+        public void Update(Role role) // update role
+        {
+            if (role.Id != 0)
+                _connection.Update<Role>(role);
+
+        }
+
+        public void Delete(Role role) // delete role
+        {
+            if (role.Id != 0)
+                _connection.Delete<Role>(role);
+
+        }
+
+        public void Delete(int id) // delete role using just the id
+        {
+            if (id != 0)
+                return;
+            var role = _connection.Get<Role>(id);
+            _connection.Delete<Role>(role);
+        }
+
+
+
     }
 }
